b74f222b2593428b85959373121c2bb6c901c8ff
Boobalan <141622627+boobal003@users.noreply.github.com>
2024-11-06 14:30:07 +0530

    updated

---
+++ app/(routes)/applications/loading.tsx
@@ -0,0 +1,13 @@
+// app/(routes)/applications/loading.tsx
+import { BeatLoader } from 'react-spinners';
+import { ContentLayout } from '@/components/layout/content-layout';
+
+export default function Loading() {
+  return (
+    <ContentLayout title='Loading...'>
+      <div className='flex items-center justify-center min-h-[400px]'>
+        <BeatLoader color='#00e902' />
+      </div>
+    </ContentLayout>
+  );
+}
