7b51b99144a462ed9f15b0f4d8a3dda31d92054c
Boobalan <141622627+boobal003@users.noreply.github.com>
2024-12-28 11:36:58 +0530

    updated

--- lib/api-keys/cors.ts
+++ lib/api-keys/cors.ts
@@ -1,8 +1,6 @@
-// lib/cors.ts
-
 export const corsHeaders = {
-  'Access-Control-Allow-Origin': '*', // For development. In production, set this to specific domains
+  'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'Access-Control-Allow-Headers':
-    'Content-Type, Authorization, X-Requested-With'
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  'Access-Control-Max-Age': '86400'
 };
